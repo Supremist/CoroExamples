1b967a364871ba37367920d9b3e8ab6cd19b64e0
Supremist <sergkarv@gmail.com>
2020-01-26 16:46:50 +0200

    Returning values via co_return. Broken

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@ struct Promise {
 	CoroHandle get_return_object() { return CoroHandle::from_promise(*this); }
 	auto initial_suspend() { return std::experimental::suspend_never(); }
 	auto final_suspend() { return std::experimental::suspend_never(); }
+	void return_value(int val) { value = val; }
+
+	int value;
 };
 
 namespace std::experimental {
@@ -21,6 +24,9 @@ Promise::CoroHandle myFirstCoroutine()
 	std::cout << "Hello" << std::endl;
 	co_await std::experimental::suspend_always();
 	std::cout << "Coroutine" << std::endl;
+	co_await std::experimental::suspend_always();
+	std::cout << "Calculating the meaning of life" << std::endl;
+	co_return 42;
 }
 
 int main(int argc, char *argv[])
@@ -28,4 +34,6 @@ int main(int argc, char *argv[])
 	auto coro = myFirstCoroutine();
 	std::cout << "Main: (doing useful work)" << std::endl;
 	coro.resume();
+	coro.resume();
+	std::cout << "The meaning of life is: " << coro.promise().value << std::endl;
 }
