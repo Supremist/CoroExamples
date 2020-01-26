73987e43b23089070826fbf516453969ec1eca83
Supremist <sergkarv@gmail.com>
2020-01-26 14:49:42 +0200

    First naive example. Broken

--- main.cpp
+++ main.cpp
@@ -1,66 +1,14 @@
 #include <iostream>
-#include <memory>
-#include <cppcoro/generator.hpp>
+#include <experimental/coroutine>
 
-struct CoroutineFrame
+void myFirstCoroutine()
 {
-	int state;
-	int current = 1, next = 1;
-};
-
-struct Resumable
-{
-	Resumable();
-
-	int next();
-
-	std::shared_ptr<CoroutineFrame> c;
-};
-
-Resumable::Resumable()
-    : c(std::make_shared<CoroutineFrame>())
-{
-	c->state = 0;
-}
-
-int Resumable::next()
-{
-#define co_yield(val)    \
-	c->state = __LINE__; \
-	return val;          \
-	case __LINE__:       \
-	;
-
-	switch(c->state) {
-	case 0:
-		while(true) {
-			co_yield(c->current)
-			auto newElem = c->current + c->next;
-			c->current = c->next;
-			c->next = newElem;
-		}
-	}
-	return 0;
-#undef co_yield
+	std::cout << "Hello" << std::endl;
+	co_await std::experimental::suspend_never();
+	std::cout << "Coroutine" << std::endl;
 }
 
 int main(int argc, char *argv[])
 {
-	Resumable coro;
-	const int fibCount = 10;
-	std::cout << "Main: First " << fibCount << " Fibonacci numbers" << std::endl;
-	for (int i = 0; i < fibCount; ++i) {
-		std::cout << coro.next() << " ";
-	}
-	std::cout << std::endl;
-
-	auto f = []() -> cppcoro::generator<float>
-	{
-	    co_yield 42.f;
-	    co_yield 101.66f;
-    };
-
-	for (auto i : f()) {
-		std::cout << i << std::endl;
-	}
+	myFirstCoroutine();
 }
