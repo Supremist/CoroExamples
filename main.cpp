f30e091cfb1d280cfba6a10c645a525fd7b3a338
Supremist <sergkarv@gmail.com>
2020-01-26 15:29:52 +0200

    Making a promise

--- main.cpp
+++ main.cpp
@@ -1,25 +1,31 @@
 #include <iostream>
 #include <experimental/coroutine>
 
+struct Promise {
+	using CoroHandle = std::experimental::coroutine_handle<Promise>;
+	CoroHandle get_return_object() { return CoroHandle::from_promise(*this); }
+	auto initial_suspend() { return std::experimental::suspend_never(); }
+	auto final_suspend() { return std::experimental::suspend_never(); }
+};
+
 namespace std::experimental {
 template<>
-struct coroutine_traits<void>{
-	struct promise_type {
-		void get_return_object() {}
-		auto initial_suspend() { return std::experimental::suspend_never(); }
-		auto final_suspend() { return std::experimental::suspend_never(); }
-	};
+struct coroutine_traits<Promise::CoroHandle> {
+	using promise_type = Promise;
 };
 }
 
-void myFirstCoroutine()
+
+Promise::CoroHandle myFirstCoroutine()
 {
 	std::cout << "Hello" << std::endl;
-	co_await std::experimental::suspend_never();
+	co_await std::experimental::suspend_always();
 	std::cout << "Coroutine" << std::endl;
 }
 
 int main(int argc, char *argv[])
 {
-	myFirstCoroutine();
+	auto coro = myFirstCoroutine();
+	std::cout << "Main: (doing useful work)" << std::endl;
+	coro.resume();
 }
