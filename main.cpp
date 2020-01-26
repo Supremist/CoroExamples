6ddcac060287fd17f3a19de513f76f4ce75aed5c
Supremist <sergkarv@gmail.com>
2020-01-26 15:04:15 +0200

    First naive example. Fixed

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include <experimental/coroutine>
 
+namespace std::experimental {
+template<>
+struct coroutine_traits<void>{
+	struct promise_type {
+		void get_return_object() {}
+		auto initial_suspend() { return std::experimental::suspend_never(); }
+		auto final_suspend() { return std::experimental::suspend_never(); }
+	};
+};
+}
+
 void myFirstCoroutine()
 {
 	std::cout << "Hello" << std::endl;
